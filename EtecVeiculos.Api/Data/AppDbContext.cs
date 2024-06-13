fccd4a70ffa4af8893da10de39e7a85ea46755af
lfcamargo1 <ulipsz11@gmail.com>
2024-06-12 22:22:29 -0300

    Atualizando erro de síntase.

--- EtecVeiculos.Api/Data/AppDbContext.cs
+++ EtecVeiculos.Api/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace EtecVeiculos.Api.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppContext> options) : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
